6297159c13f7719951cf89e852e61584edfbacda
Lavhran <lavhran@gmail.com>
2022-03-09 09:51:13 +0100

    Added slider dialog on right click

--- gui_components/slider.py
+++ gui_components/slider.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.simpledialog import askinteger
 from .bar import Bar
 
 
@@ -7,6 +8,7 @@ class Slider(Bar):
         super().__init__(master, range, colour, haslabel=haslabel, font=font, **kwargs)
 
         self.bind('<Button-1>', self.moved)
+        self.bind('<Button-3>', self.set_value_dialog)
         self.bind('<B1-Motion>', self.moved)
 
     def moved(self, event) -> None:
@@ -17,3 +19,8 @@ class Slider(Bar):
             value = self.width
 
         self.change_value(round(value / self.width * self.range))
+
+    def set_value_dialog(self, event) -> None:
+        value = askinteger("Slider", "Set slider to")
+        if value:
+            self.change_value(value)
